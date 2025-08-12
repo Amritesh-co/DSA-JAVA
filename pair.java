357b403136c2eef0f48e6b03d4ad433cc9eee01d
Amritesh Sahu <amriteshsahu96@gmail.com>
2025-08-12 10:47:32 +0530

    pairs of the element of the array

--- pair.java
+++ pair.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class pair {
-    void pairs(int arr[],int start){
+    void pairs(int arr[]){
         for(int i=0;i<arr.length-1;i++){
             for(int j=(i+1);j<arr.length;j++){
                System.out.print("(" + arr[i] + "," + arr[j] +")");
@@ -10,6 +10,8 @@ public class pair {
     }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        
+        int arr[]={10,12,14,16,18,21,45};
+        pair p= new pair();
+        p.pairs(arr);
     }
 }
